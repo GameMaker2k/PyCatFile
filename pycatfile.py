51270b6c7341391263f505458c14f69ee23c754a
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2018-02-26 23:43:41 -0600

    Add files via upload

--- pycatfile.py
+++ pycatfile.py
@@ -4,14 +4,17 @@
 '''
     This program is free software; you can redistribute it and/or modify
     it under the terms of the Revised BSD License.
-    This program is distributed in the hope that it will be useful,
+
+	This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     Revised BSD License for more details.
-    Copyright 2018 Cool Dude 2k - http://idb.berlios.de/
+
+	Copyright 2018 Cool Dude 2k - http://idb.berlios.de/
     Copyright 2018 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
-    $FileInfo: pycatfile.py - Last Update: 2/26/2018 Ver. 0.0.1 RC 1 - Author: cooldude2k $
+
+	$FileInfo: pycatfile.py - Last Update: 2/26/2018 Ver. 0.0.1 RC 1 - Author: cooldude2k $
 '''
 
 __program_name__ = "PyCatFile";
