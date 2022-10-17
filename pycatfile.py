e50fdf5748d41c48c044b033606b1fb4cf84b841
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2022-10-17 14:00:37 -0500

    Update pycatfile.py

--- pycatfile.py
+++ pycatfile.py
@@ -14,7 +14,7 @@
     Copyright 2018-2022 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2022 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: pycatfile.py - Last Update: 9/16/2022 Ver. 0.0.1 RC 1 - Author: cooldude2k $
+    $FileInfo: pycatfile.py - Last Update: 10/17/2022 Ver. 0.0.1 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals;
@@ -71,7 +71,7 @@ __program_name__ = "PyCatFile";
 __project__ = __program_name__;
 __project_url__ = "https://github.com/GameMaker2k/PyCatFile";
 __version_info__ = (0, 0, 1, "RC 1", 1);
-__version_date_info__ = (2022, 9, 16, "RC 1", 1);
+__version_date_info__ = (2022, 10, 17, "RC 1", 1);
 __version_date__ = str(__version_date_info__[0]) + "." + str(__version_date_info__[1]).zfill(2) + "." + str(__version_date_info__[2]).zfill(2);
 __revision__ = __version_info__[3];
 __revision_id__ = "$Id$";
