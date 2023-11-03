1ea92f7bce0d209218cbb20d1b4c4d01526f5411
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-11-02 20:41:16 -0500

    Create pycatfilealt_test.py

---
+++ pycatfilealt_test.py
@@ -0,0 +1,24 @@
+import unittest
+from catfilepacker import CatFilePacker  # Assuming the script above is named catfilepacker.py
+import os
+
+class TestCatFilePacker(unittest.TestCase):
+    def setUp(self):
+        self.packer = CatFilePacker(checksum_type='crc32')
+        self.test_tar_path = 'test.tar'
+        self.test_catfile_path = 'test.cat'
+
+    def test_pack_from_tar(self):
+        # Implement this test with actual file operations or mocking
+        pass
+
+    def test_create_metadata(self):
+        # Implement this test with actual member data or mocking
+        pass
+
+    def test_calculate_checksum(self):
+        # Implement this test with known data and checksums
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
