37126ee58abd388fe73d9c5b08555d3e56aa355d
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-02-21 20:37:57 -0600

    Update catfile.py

--- catfile.py
+++ catfile.py
@@ -21,38 +21,25 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os, sys, logging, argparse, pycatfile;
 from io import open as open;
 
-if(sys.version[0]=="2"):
+teststringio = 0;
+if(teststringio<=0):
  try:
-  from io import StringIO, BytesIO;
+  from cStringIO import StringIO as BytesIO;
+  teststringio = 1;
  except ImportError:
-  try:
-   from cStringIO import StringIO;
-   from cStringIO import StringIO as BytesIO;
-  except ImportError:
-   from StringIO import StringIO;
-   from StringIO import StringIO as BytesIO;
-elif(sys.version[0]>="3"):
- from io import StringIO, BytesIO;
-else:
- teststringio = 0;
- if(teststringio<=0):
-  try:
-   from cStringIO import StringIO as BytesIO;
-   teststringio = 1;
-  except ImportError:
-   teststringio = 0;
- if(teststringio<=0):
-  try:
-   from StringIO import StringIO as BytesIO;
-   teststringio = 2;
-  except ImportError:
-   teststringio = 0;
- if(teststringio<=0):
-  try:
-   from io import BytesIO;
-   teststringio = 3;
-  except ImportError:
-   teststringio = 0;
+  teststringio = 0;
+if(teststringio<=0):
+ try:
+  from StringIO import StringIO as BytesIO;
+  teststringio = 2;
+ except ImportError:
+  teststringio = 0;
+if(teststringio<=0):
+ try:
+  from io import BytesIO;
+  teststringio = 3;
+ except ImportError:
+  teststringio = 0;
 
 __project__ = pycatfile.__project__;
 __program_name__ = pycatfile.__program_name__;
@@ -60,91 +47,52 @@ __project_url__ = pycatfile.__project_url__;
 __version_info__ = pycatfile.__version_info__;
 __version_date_info__ = pycatfile.__version_date_info__;
 __version_date__ = pycatfile.__version_date__;
-__version_date_plusrc__ = pycatfile.__version_date_plusrc__
-__version__ = pycatfile.__version__;
 __version_date_plusrc__ = pycatfile.__version_date_plusrc__;
+__version__ = pycatfile.__version__;
 __cat_header_ver__ = pycatfile.__cat_header_ver__;
 
-argparser = argparse.ArgumentParser(description="Manipulating concatenate files", conflict_handler="resolve", add_help=True);
-argparser.add_argument("-V", "--version", action="version", version=__program_name__ + " " + __version__);
-argparser.add_argument("-i", "-f", "--input", help="files to concatenate or concatenate file extract", required=True);
-argparser.add_argument("-d", "-v", "--verbose", action="store_true", help="print various debugging information");
-argparser.add_argument("-c", "--create", action="store_true", help="concatenate files only");
-argparser.add_argument("-checksum", "--checksum", default="crc32", help="checksum type to use default is crc32");
-argparser.add_argument("-e", "-x", "--extract", action="store_true", help="extract files only");
-argparser.add_argument("-l", "-t", "--list", action="store_true", help="list files only");
-argparser.add_argument("-r", "--repack", action="store_true", help="reconcatenate files only fixing checksum errors");
-argparser.add_argument("-o", "--output", default=None, help="extract concatenate files to or concatenate output name");
-argparser.add_argument("-compression", "--compression", default="auto", help="concatenate files with compression");
-argparser.add_argument("-level", "--level", default=None, help="concatenate files with compression level");
-argparser.add_argument("-t", "--converttar", action="store_true", help="convert tar file to catfile");
-argparser.add_argument("-z", "--convertzip", action="store_true", help="convert tar file to catfile");
-argparser.add_argument("-T", "--text", action="store_true", help="read file locations from text file");
+argparser = argparse.ArgumentParser(description="Manipulate concatenated files.", conflict_handler="resolve", add_help=True);
+argparser.add_argument("-V", "--version", action="version", version=f"{__program_name__} {__version__}");
+argparser.add_argument("-i", "-f", "--input", help="Specify the file(s) to concatenate or the concatenated file to extract.", required=True);
+argparser.add_argument("-d", "-v", "--verbose", action="store_true", help="Enable verbose mode to display various debugging information.");
+argparser.add_argument("-c", "--create", action="store_true", help="Perform concatenation operation only.");
+argparser.add_argument("-checksum", "--checksum", default="crc32", help="Specify the type of checksum to use. Default is crc32.");
+argparser.add_argument("-e", "-x", "--extract", action="store_true", help="Perform extraction operation only.");
+argparser.add_argument("-l", "-t", "--list", action="store_true", help="List files included in the concatenated file.");
+argparser.add_argument("-r", "--repack", action="store_true", help="Re-concatenate files, fixing checksum errors if any.");
+argparser.add_argument("-o", "--output", default=None, help="Specify the name for the extracted concatenated files or the output concatenated file.");
+argparser.add_argument("-compression", "--compression", default="auto", help="Specify the compression method to use for concatenation.");
+argparser.add_argument("-level", "--level", default=None, help="Specify the compression level for concatenation.");
+argparser.add_argument("-t", "--converttar", action="store_true", help="Convert a tar file to a catfile.");
+argparser.add_argument("-z", "--convertzip", action="store_true", help="Convert a zip file to a catfile.");
+argparser.add_argument("-T", "--text", action="store_true", help="Read file locations from a text file.");
 getargs = argparser.parse_args();
 
-should_extract = False;
-should_create = True;
-should_list = False;
-should_convert_tar = False;
-should_convert_zip = False;
-if(not getargs.extract and getargs.create and not getargs.list):
- should_create = True;
- should_extract = False;
- should_list = False;
-if(getargs.extract and not getargs.create and not getargs.list):
- should_create = False;
- should_extract = True;
- should_list = False;
-if(getargs.extract and getargs.create and not getargs.list):
- should_create = True;
- should_extract = False;
- should_list = False;
-if(not getargs.extract and not getargs.create and not getargs.list):
- should_create = True;
- should_extract = False;
- should_list = False;
-if(not getargs.extract and getargs.create and getargs.list):
- should_create = True;
- should_extract = False;
- should_list = False;
-if(getargs.extract and not getargs.create and getargs.list):
- should_create = False;
- should_extract = True;
- should_list = False;
-if(getargs.extract and getargs.create and getargs.list):
- should_create = True;
- should_extract = False;
- should_list = False;
-if(not getargs.extract and not getargs.create and getargs.list):
- should_create = False;
- should_extract = False;
- should_list = True;
-should_repack = False;
-if(should_create and getargs.repack):
- should_repack = True;
-if(getargs.converttar):
- should_convert_tar = True;
- should_convert_zip = False;
-if(getargs.convertzip and not should_convert_tar):
- should_convert_zip = True;
- should_convert_tar = False;
-if(getargs.verbose):
- logging.basicConfig(format="%(message)s", stream=sys.stdout, level=logging.DEBUG);
-if(should_create and not should_extract and not should_list and not should_repack):
- if(should_convert_tar):
+# Determine actions based on user input
+should_create = getargs.create and not getargs.extract and not getargs.list;
+should_extract = getargs.extract and not getargs.create and not getargs.list;
+should_list = getargs.list and not getargs.create and not getargs.extract;
+should_repack = getargs.create and getargs.repack;
+
+# Execute the appropriate functions based on determined actions and arguments
+if should_create:
+ if getargs.converttar:
   pycatfile.PackCatFileFromTarFile(getargs.input, getargs.output, getargs.compression, getargs.level, getargs.checksum, [], getargs.verbose, False);
- elif(should_convert_zip and not should_convert_tar):
+ elif getargs.convertzip:
   pycatfile.PackCatFileFromZipFile(getargs.input, getargs.output, getargs.compression, getargs.level, getargs.checksum, [], getargs.verbose, False);
  else:
   pycatfile.PackCatFile(getargs.input, getargs.output, getargs.text, getargs.compression, getargs.level, False, getargs.checksum, [], getargs.verbose, False);
-if(should_create and not should_extract and not should_list and should_repack):
+
+if should_repack:
  pycatfile.RePackCatFile(getargs.input, getargs.output, getargs.compression, getargs.level, False, getargs.checksum, False, [], getargs.verbose, False);
-if(not should_create and should_extract and not should_list):
+
+if should_extract:
  pycatfile.UnPackCatFile(getargs.input, getargs.output, False, False, getargs.verbose, False);
-if(not should_create and not should_extract and should_list):
- if(should_convert_tar):
+
+if should_list:
+ if getargs.converttar:
   pycatfile.TarFileListFiles(getargs.input, getargs.verbose, False);
- elif(should_convert_zip and not should_convert_tar):
+ elif getargs.convertzip:
   pycatfile.ZipFileListFiles(getargs.input, getargs.verbose, False);
  else:
   pycatfile.CatFileListFiles(getargs.input, 0, 0, False, getargs.verbose, False);
