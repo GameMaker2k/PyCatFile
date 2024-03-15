220460e99a9c26ee6b5b9896302dec10e0a0ad6f
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-03-15 08:40:28 -0700

    Add files via upload

--- mkcatfilebundle.py
+++ mkcatfilebundle.py
@@ -14,7 +14,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: mkbundle.py - Last Update: 3/14/2024 Ver. 0.4.6 RC 1 - Author: cooldude2k $
+    $FileInfo: mkbundle.py - Last Update: 3/14/2024 Ver. 0.5.0 RC 1 - Author: cooldude2k $
 '''
 
 import os, sys, shutil, subprocess, tempfile, subprocess, platform;
