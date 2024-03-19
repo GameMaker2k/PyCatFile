3d088e92ad6835c2f65e6f897a64ad9661e39766
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-03-19 17:18:26 -0500

    Add files via upload

--- catfile.py
+++ catfile.py
@@ -18,8 +18,7 @@
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals;
-import sys, argparse, pycatfile, binascii;
-from io import open as open;
+import argparse, pycatfile, binascii;
 
 rarfile_support = pycatfile.rarfile_support;
 
