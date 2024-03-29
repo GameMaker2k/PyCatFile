73df5f199d99ef1247d70f124313f33b296e80d3
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-03-29 14:10:02 -0500

    Add files via upload

--- mkcatfilebundle.py
+++ mkcatfilebundle.py
@@ -14,7 +14,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: mkbundle.py - Last Update: 3/27/2024 Ver. 0.6.8 RC 1 - Author: cooldude2k $
+    $FileInfo: mkbundle.py - Last Update: 3/29/2024 Ver. 0.7.0 RC 1 - Author: cooldude2k $
 '''
 
 import os, sys, shutil, subprocess, tempfile, subprocess, platform;
--- pycatfile.py
+++ pycatfile.py
@@ -14,7 +14,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: pycatfile.py - Last Update: 3/27/2024 Ver. 0.6.8 RC 1 - Author: cooldude2k $
+    $FileInfo: pycatfile.py - Last Update: 3/29/2024 Ver. 0.7.0 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals;
@@ -162,7 +162,7 @@ __use_new_style__ = True;
 __file_format_list__ = [__file_format_name__, __file_format_magic__, __file_format_lower__, __file_format_len__, __file_format_hex__, __file_format_delimiter__, __file_format_ver__, __use_new_style__];
 __project__ = __program_name__;
 __project_url__ = "https://github.com/GameMaker2k/PyCatFile";
-__version_info__ = (0, 6, 8, "RC 1", 1);
+__version_info__ = (0, 7, 0, "RC 1", 1);
 __version_date_info__ = (2024, 3, 27, "RC 1", 1);
 __version_date__ = str(__version_date_info__[0]) + "." + str(__version_date_info__[1]).zfill(2) + "." + str(__version_date_info__[2]).zfill(2);
 __revision__ = __version_info__[3];
