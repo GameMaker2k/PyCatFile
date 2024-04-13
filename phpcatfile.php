ed2f7b9b7585c8042d3753b7dfee0b189cb3f0f3
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-04-12 20:25:41 -0500

    Add files via upload

--- phpcatfile.php
+++ phpcatfile.php
@@ -12,7 +12,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: phpcatfile.php - Last Update: 4/10/2024 Ver. 0.7.6 RC 1 - Author: cooldude2k $
+    $FileInfo: phpcatfile.php - Last Update: 4/12/2024 Ver. 0.7.8 RC 1 - Author: cooldude2k $
 */
 
 date_default_timezone_set('UTC');
