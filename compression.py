ab14cc56f5d4e1c6037416610924130b3a4786bb
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-05-02 16:58:03 -0500

    Add files via upload

--- compression.py
+++ compression.py
@@ -14,7 +14,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: compression.py - Last Update: 5/1/2024 Ver. 0.9.6 RC 1 - Author: cooldude2k $
+    $FileInfo: compression.py - Last Update: 5/2/2024 Ver. 0.9.8 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals;
