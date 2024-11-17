75e51e6bf4defacfa129c0726f0d4afc4cd6fb59
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-11-17 16:25:54 -0600

    Update pycatfile.py

--- pycatfile.py
+++ pycatfile.py
@@ -210,7 +210,7 @@ except ImportError:
 __use_pysftp__ = False
 __use_alt_format__ = False
 scriptconf = os.path.join(os.path.dirname(get_importing_script_path()), "catfile.ini")
-if os.path.exists(scriptconf)
+if os.path.exists(scriptconf):
     __config_file__ = scriptconf
 else:
     __config_file__ = os.path.join(os.path.dirname(os.path.realpath(__file__)), "catfile.ini")
