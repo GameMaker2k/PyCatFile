ebf2e4104485506e96f717a4ba20ce90deeaf29a
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-12-04 20:00:08 -0600

    Update altcatfile.py

--- altcatfile.py
+++ altcatfile.py
@@ -17,6 +17,7 @@
     $FileInfo: altcatfile.py - Last Update: 12/4/2024 Ver. 0.14.18 RC 1 - Author: cooldude2k $
 '''
 
+
 from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
 import os
 import argparse
