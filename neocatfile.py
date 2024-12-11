fd5f8c3282092acc3f3bfe3354c8ae642ae195de
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-12-10 20:37:52 -0600

    Small update

--- neocatfile.py
+++ neocatfile.py
@@ -14,7 +14,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: neocatfile.py - Last Update: 12/6/2024 Ver. 0.15.0 RC 1 - Author: cooldude2k $
+    $FileInfo: neocatfile.py - Last Update: 12/10/2024 Ver. 0.15.2 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
