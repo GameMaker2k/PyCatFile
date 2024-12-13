1d004bb06cee6eddc8feb662d9ed8944c834cec9
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-12-13 14:20:09 -0600

    Small update

--- pyshell-old.py
+++ pyshell-old.py
@@ -13,7 +13,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: pyshell-old.py - Last Update: 12/11/2024 Ver. 0.15.4 RC 1 - Author: cooldude2k $
+    $FileInfo: pyshell-old.py - Last Update: 12/13/2024 Ver. 0.15.6 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
