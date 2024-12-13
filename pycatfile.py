61fd380fb81698be660d2ecfb142afeffcc634ea
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-12-13 12:17:58 -0600

    Update pycatfile.py

--- pycatfile.py
+++ pycatfile.py
@@ -310,6 +310,7 @@ __version_date__ = str(__version_date_info__[0]) + "." + str(
     __version_date_info__[1]).zfill(2) + "." + str(__version_date_info__[2]).zfill(2)
 __revision__ = __version_info__[3]
 __revision_id__ = "$Id$"
+
 if(__version_info__[4] is not None):
     __version_date_plusrc__ = __version_date__ + \
         "-" + str(__version_date_info__[4])
