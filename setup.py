63e8d1be74837fad2ee889a016008bc8f1104c11
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2025-02-07 19:44:51 -0600

    Version up ^_^

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
     Copyright 2016-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2016-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: setup.py - Last Update: 1/31/2025 Ver. 0.18.0 RC 1 - Author: cooldude2k $
+    $FileInfo: setup.py - Last Update: 2/7/2025 Ver. 0.18.2 RC 1 - Author: cooldude2k $
 '''
 
 import os
