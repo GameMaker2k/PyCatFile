7fb62044ad02039c51388d10cb8ee28895633693
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2025-10-01 20:10:13 -0500

    Version up day ^_^

--- catfile.py
+++ catfile.py
@@ -14,7 +14,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: catfile.py - Last Update: 8/29/2025 Ver. 0.22.4 RC 1 - Author: cooldude2k $
+    $FileInfo: catfile.py - Last Update: 10/1/2025 Ver. 0.23.0 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
