655fae50e203ac9f69d7baf56d43d44b4ca6f72f
和月猫少年くん Kazuki Neko-kun 🌙🐱 かづきねこしょうねんくん <kazuki.suzuki.wa@gmail.com>
2025-11-03 23:21:19 -0600

    Add files via upload

--- pycatfile.py
+++ pycatfile.py
@@ -4906,7 +4906,7 @@ def ReadFileHeaderDataWithContentToArray(fp, listonly=False, contentasfile=True,
         pyhascontents = False
     fcontents.seek(0, 0)
     newfccs = GetFileChecksum(
-        version = "0.24.6" HeaderOut[-3].lower(), False, formatspecs)
+        fcontents.read(), HeaderOut[-3].lower(), False, formatspecs)
     fcontents.seek(0, 0)
     if(fccs != newfccs and not skipchecksum and not listonly):
         VerbosePrintOut("File Content Checksum Error with file " +
