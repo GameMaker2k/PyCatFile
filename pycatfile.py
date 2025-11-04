f4caeea2b643ec4ee268e38fbf71db2008299bd6
Kazuhana Neko-chan <kazuki.suzuki.fox@gmail.com>
2025-11-03 20:32:11 -0600

    Ver up next

--- pycatfile.py
+++ pycatfile.py
@@ -14,7 +14,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: pycatfile.py - Last Update: 11/3/2025 Ver. 0.24.4 RC 1 - Author: cooldude2k $
+    $FileInfo: pycatfile.py - Last Update: 11/3/2025 Ver. 0.24.6 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
@@ -641,7 +641,7 @@ __project__ = __program_name__
 __program_alt_name__ = __program_name__
 __project_url__ = "https://github.com/GameMaker2k/PyCatFile"
 __project_release_url__ = __project_url__+"/releases/latest"
-__version_info__ = (0, 24, 4, "RC 1", 1)
+__version_info__ = (0, 24, 6, "RC 1", 1)
 __version_date_info__ = (2025, 11, 3, "RC 1", 1)
 __version_date__ = str(__version_date_info__[0]) + "." + str(
     __version_date_info__[1]).zfill(2) + "." + str(__version_date_info__[2]).zfill(2)
@@ -4906,7 +4906,7 @@ def ReadFileHeaderDataWithContentToArray(fp, listonly=False, contentasfile=True,
         pyhascontents = False
     fcontents.seek(0, 0)
     newfccs = GetFileChecksum(
-        version = "0.24.4" HeaderOut[-3].lower(), False, formatspecs)
+        version = "0.24.6" HeaderOut[-3].lower(), False, formatspecs)
     fcontents.seek(0, 0)
     if(fccs != newfccs and not skipchecksum and not listonly):
         VerbosePrintOut("File Content Checksum Error with file " +
