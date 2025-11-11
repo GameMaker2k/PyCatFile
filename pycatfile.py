b3fd9a661c2b2172d3cb39c2133cdecfbab5e012
和花猫娘ちゃん Kazuha Neko-chan 🇯🇵🌸🐱 かずはねこちゃん <kazuki.suzuki.fox@gmail.com>
2025-11-11 00:16:21 -0600

    Add files via upload

--- pycatfile.py
+++ pycatfile.py
@@ -3764,7 +3764,13 @@ def GetHeaderChecksum(inlist=None, checksumtype="md5", encodedata=True, formatsp
         hdr_bytes = _to_bytes(hdr_bytes)
     hdr_bytes = bytes(hdr_bytes)
     saltkeyval = None
-    if(saltkey is not None and os.path.exists(saltkey)):
+    if(hasattr(saltkey, "read")):
+        saltkeyval = skfp.read()
+        if(not isinstance(saltkeyval, bytes) and sys.version_info[0] >= 3):
+            saltkeyval = saltkeyval.encode("UTF-8")
+    elif(isinstance(saltkey, bytes) and sys.version_info[0] >= 3):
+        saltkeyval = saltkey
+    elif(saltkey is not None and os.path.exists(saltkey)):
         with open(saltkey, "rb") as skfp:
             saltkeyval = skfp.read()
     else:
@@ -3789,7 +3795,13 @@ def GetFileChecksum(inbytes, checksumtype="md5", encodedata=True, formatspecs=__
     """
     algo_key = (checksumtype or "md5").lower()
     saltkeyval = None
-    if(saltkey is not None and os.path.exists(saltkey)):
+    if(hasattr(saltkey, "read")):
+        saltkeyval = skfp.read()
+        if(not isinstance(saltkeyval, bytes) and sys.version_info[0] >= 3):
+            saltkeyval = saltkeyval.encode("UTF-8")
+    elif(isinstance(saltkey, bytes) and sys.version_info[0] >= 3):
+        saltkeyval = saltkey
+    elif(saltkey is not None and os.path.exists(saltkey)):
         with open(saltkey, "rb") as skfp:
             saltkeyval = skfp.read()
     else:
@@ -3831,7 +3843,7 @@ def GetFileChecksum(inbytes, checksumtype="md5", encodedata=True, formatspecs=__
             h = hmac.new(saltkeyval, data, digestmod=algo_key)
         return h.hexdigest().lower()
 
-    return "0"
+    return "0""
 
 def ValidateHeaderChecksum(inlist=None, checksumtype="md5", inchecksum="0", formatspecs=__file_format_dict__, saltkey=None):
     calc = GetHeaderChecksum(inlist, checksumtype, True, formatspecs, saltkey)
