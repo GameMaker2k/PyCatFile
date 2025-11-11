47195c431379daab68e259ca46b4d5415d20bf4c
和花猫娘ちゃん Kazuha Neko-chan 🇯🇵🌸🐱 かずはねこちゃん <kazuki.suzuki.fox@gmail.com>
2025-11-10 22:50:39 -0600

    Add files via upload

--- pycatfile.py
+++ pycatfile.py
@@ -3765,9 +3765,8 @@ def GetHeaderChecksum(inlist=None, checksumtype="md5", encodedata=True, formatsp
     hdr_bytes = bytes(hdr_bytes)
     saltkeyval = None
     if(saltkey is not None and os.path.exists(saltkey)):
-        skfp = open(saltkey, "rb")
-        saltkeyval = skfp.read()
-        skfp.close()
+        with open(saltkey, "rb") as skfp:
+            saltkeyval = skfp.read()
     else:
         saltkey = None
     if(saltkeyval is None):
@@ -3791,9 +3790,8 @@ def GetFileChecksum(inbytes, checksumtype="md5", encodedata=True, formatspecs=__
     algo_key = (checksumtype or "md5").lower()
     saltkeyval = None
     if(saltkey is not None and os.path.exists(saltkey)):
-        skfp = open(saltkey, "rb")
-        saltkeyval = skfp.read()
-        skfp.close()
+        with open(saltkey, "rb") as skfp:
+            saltkeyval = skfp.read()
     else:
         saltkey = None
     if(saltkeyval is None):
