5c9fa6180a14fc27e3c40ec69dae3498ecddcb12
和陽菜狐娘ちゃん Kazuhina Kitsune-chan 🇯🇵☀️🦊 かずひなきつねちゃん <kazuki.suzuki.cat@gmail.com>
2025-11-15 23:31:00 -0600

    Add files via upload

--- catfile.py
+++ catfile.py
@@ -14,7 +14,7 @@
     Copyright 2018-2024 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2018-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: catfile.py - Last Update: 11/15/2025 Ver. 0.27.2 RC 1 - Author: cooldude2k $
+    $FileInfo: catfile.py - Last Update: 11/16/2025 Ver. 0.27.4 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
