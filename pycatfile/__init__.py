b46444f16072f17071d710fc6ef919c4adefaa2e
Tsukiumi Neko-kun <kazuki.suzuki.wa@gmail.com>
2026-02-24 14:11:09 -0600

    Small update

--- pycatfile/__init__.py
+++ pycatfile/__init__.py
@@ -20,22 +20,22 @@
 from .pycatfile import *
 from . import pycatfile
 
-__project__ = pycatfile__project__
-__program_name__ = pycatfile__program_name__
-__file_format_name__ = pycatfile__file_format_name__
-__file_format_magic__ = pycatfile__file_format_magic__
-__file_format_len__ = pycatfile__file_format_len__
-__file_format_hex__ = pycatfile__file_format_hex__
-__file_format_delimiter__ = pycatfile__file_format_delimiter__
-__file_format_dict__ = pycatfile__file_format_dict__
-__file_format_default__ = pycatfile__file_format_default__
-__file_format_multi_dict__ = pycatfile__file_format_multi_dict__
-__use_new_style__ = pycatfile__use_new_style__
-__use_advanced_list__ = pycatfile__use_advanced_list__
-__use_alt_inode__ = pycatfile__use_alt_inode__
-__project_url__ = pycatfile__project_url__
-__version_info__ = pycatfile__version_info__
-__version_date_info__ = pycatfile__version_date_info__
-__version_date__ = pycatfile__version_date__
-__version_date_plusrc__ = pycatfile__version_date_plusrc__
-__version__ = pycatfile__version__
+__project__ = pycatfile.__project__
+__program_name__ = pycatfile.__program_name__
+__file_format_name__ = pycatfile.__file_format_name__
+__file_format_magic__ = pycatfile.__file_format_magic__
+__file_format_len__ = pycatfile.__file_format_len__
+__file_format_hex__ = pycatfile.__file_format_hex__
+__file_format_delimiter__ = pycatfile.__file_format_delimiter__
+__file_format_dict__ = pycatfile.__file_format_dict__
+__file_format_default__ = pycatfile.__file_format_default__
+__file_format_multi_dict__ = pycatfile.__file_format_multi_dict__
+__use_new_style__ = pycatfile.__use_new_style__
+__use_advanced_list__ = pycatfile.__use_advanced_list__
+__use_alt_inode__ = pycatfile.__use_alt_inode__
+__project_url__ = pycatfile.__project_url__
+__version_info__ = pycatfile.__version_info__
+__version_date_info__ = pycatfile.__version_date_info__
+__version_date__ = pycatfile.__version_date__
+__version_date_plusrc__ = pycatfile.__version_date_plusrc__
+__version__ = pycatfile.__version__
